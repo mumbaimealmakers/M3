e2b307521547d44ce615927545a46c7a2604c8ca
ravalravi103 <ravalravi103@gmail.com>
2025-02-13 12:21:26 +0530

    config file changes done

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/M3',
+  base: 'https://github.com/mumbaimealmakers/M3',
   plugins: [react()],
 })
