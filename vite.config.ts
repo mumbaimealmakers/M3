3245d059b549fe397a7e324e9be76721c265cab7
ravalravi103 <ravalravi103@gmail.com>
2025-02-13 13:01:58 +0530

    added server option in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/mumbaimealmakers/M3',
+  base: '/M3',
   plugins: [react()],
+  server: {
+    cors: true
+  }
 })
